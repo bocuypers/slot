fd9302686397002e6f9cdab443d420919842f8b6
bocuypers <123274149+bocuypers@users.noreply.github.com>
2023-05-09 13:07:03 +0200

    SLO123

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    code += 0
+    code += 1
     start += 0
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
@@ -7,21 +7,20 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     start += 0
 })
 input.onButtonPressed(Button.B, function () {
-    code += 0
+    code += 1
     start += 0
 })
 let start = 1
-let code = 1
+let code = 0
 basic.forever(function () {
     if (code == 1 || start == 1) {
         basic.showString("c")
     } else if (code == 0 || start == 0) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+        basic.clearScreen()
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        code += 1
     }
 })
