c194b0862b4653f042da402dcaf7cd0eace696c2
bocuypers <123274149+bocuypers@users.noreply.github.com>
2023-05-12 13:55:32 +0200

    Slot12345

--- main.ts
+++ main.ts
@@ -1,26 +1,35 @@
 input.onButtonPressed(Button.A, function () {
     code += 1
-    start += 0
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    code += 0
-    start += 0
+	
 })
 input.onButtonPressed(Button.B, function () {
-    code += 1
-    start += 0
+	
 })
-let start = 1
 let code = 0
-basic.forever(function () {
-    if (code == 1 || start == 1) {
-        basic.showString("c")
-    } else if (code == 0 || start == 0) {
-        basic.clearScreen()
-    }
-})
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         code += 1
+    }
+})
+basic.forever(function () {
+    if (code >= 1) {
+        basic.clearScreen()
+    } else {
+        basic.showString("c")
+    }
+})
+basic.forever(function () {
+    if (code == 1) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            `)
+    } else if (code == 0) {
+        basic.showString("c")
     }
 })
