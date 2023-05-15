31c111833af0c3b1cee223f4cc6a3f56a5f5d168
bocuypers <123274149+bocuypers@users.noreply.github.com>
2023-05-15 09:35:58 +0200

    9slot

--- main.ts
+++ main.ts
@@ -1,25 +1,101 @@
-input.onButtonPressed(Button.A, function () {
-    code += 1
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
 let code = 0
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        code += 1
+    if (code == 0 && input.buttonIsPressed(Button.A)) {
+        basic.pause(200)
+        code = 1
+    }
+    if (code == 1 && input.buttonIsPressed(Button.B)) {
+        basic.pause(200)
+        code = 2
+    }
+    if (code == 2 && input.logoIsPressed()) {
+        basic.pause(200)
+        code = 3
+    }
+    if (code == 3 && input.buttonIsPressed(Button.A)) {
+        basic.pause(200)
+        code = 4
+    }
+    if (code == 4 && input.logoIsPressed()) {
+        basic.pause(200)
+        code = 5
     }
 })
 basic.forever(function () {
-    if (code >= 1) {
-        basic.clearScreen()
-    } else {
+    if (code == 0 && (input.logoIsPressed() || input.buttonIsPressed(Button.B))) {
+        code = 9
+    }
+    if (code == 1 && (input.buttonIsPressed(Button.A) || input.logoIsPressed())) {
+        code = 9
+    }
+    if (code == 2 && (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
+        code = 9
+    }
+    if (code == 3 && (input.logoIsPressed() || input.buttonIsPressed(Button.B))) {
+        code = 9
+    }
+    if (code == 4 && (input.buttonIsPressed(Button.B) || input.buttonIsPressed(Button.A))) {
+        code = 9
+    }
+})
+basic.forever(function () {
+    if (code == 0) {
         basic.showString("c")
     }
-})
-basic.forever(function () {
-	
+    if (code == 1) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            `)
+    }
+    if (code == 2) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            `)
+    }
+    if (code == 3) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # . .
+            `)
+    }
+    if (code == 4) {
+        basic.showLeds(`
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            `)
+    }
+    if (code == 5) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (code == 9) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.pause(200)
+        code = 0
+    }
 })
